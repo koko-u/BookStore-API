b5591f87b2b1f7872a628204a1fdad828e77e594
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2020-08-29 09:22:48 +0900

    project setup and configuration

---
+++ BookStore-API/Services/LoggerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using NLog;
+
+namespace BookStore_API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <inheritdoc />
+        public void LogInfo(string message)
+        {
+            Logger.Info(message);
+        }
+
+        /// <inheritdoc />
+        public void LogWarn(string message)
+        {
+            Logger.Warn(message);
+        }
+
+        /// <inheritdoc />
+        public void LogDebug(string message)
+        {
+            Logger.Debug(message);
+        }
+
+        /// <inheritdoc />
+        public void LogError(string message)
+        {
+            Logger.Error(message);
+        }
+    }
+}
